2da756f9ed44d71a964060f60937ef7ddd18e7ba
romo <robmoore121@gmail.com>
2020-12-18 11:41:47 +0800

    Add script to bounce Jenkins

---
+++ Deployment/jenkins_bounce.sh
@@ -0,0 +1,6 @@
+NODE_IP=$(./node_ip.sh)
+ssh -i jenkins_server_keys.pem "ubuntu@${NODE_IP}" << 'EOF'
+  PORT=$(lsof -t -i :8080)
+  kill $PORT
+  sudo docker run -d --network host -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+EOF
