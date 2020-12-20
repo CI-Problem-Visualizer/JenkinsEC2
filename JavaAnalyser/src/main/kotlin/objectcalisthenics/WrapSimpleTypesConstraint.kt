ef9124b8582a27978a98fdd0ecb30720cf7c433b
romo <robmoore121@gmail.com>
2020-12-20 17:38:01 +0800

    Return AllFine() from WrapSimpleTypesConstraint for now

--- JavaAnalyser/src/main/kotlin/objectcalisthenics/WrapSimpleTypesConstraint.kt
+++ JavaAnalyser/src/main/kotlin/objectcalisthenics/WrapSimpleTypesConstraint.kt
@@ -1,12 +1,12 @@
 package objectcalisthenics
 
 import JavaFile
+import analyser.AllFine
 import analyser.CodeAnalysis
 import analyser.JavaFileFeedback
-import analyser.RoomForImprovement
 
 class WrapSimpleTypesConstraint : CodeAnalysis {
     override fun evaluate(javaFile: JavaFile): JavaFileFeedback {
-        return RoomForImprovement("Wrap primitives and Strings")
+        return AllFine()
     }
 }
