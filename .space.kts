564f7b54e44db4c2cd427110104427f81079f1ab
romo <robmoore121@gmail.com>
2021-01-05 07:45:53 +0800

    Push initial .space.kts file

---
+++ .space.kts
@@ -0,0 +1,24 @@
+/**
+ * cd JavaAnalyser
+ * ./gradlew build
+ * cd ../Deployment
+ * if (node_ip.sh != empty):
+ *   ./analysis_server_bounce.sh
+ * else:
+ *   Bring up the node
+ * */
+job("Analysis Server") {
+    container("openjdk:11") {
+        resources {
+            memory = 2048
+            cpu = 2048
+        }
+
+        shellScript{
+            content = """
+                cd JavaAnalyser
+                ./gradlew build
+            """.trimIndent()
+        }
+    }
+}
