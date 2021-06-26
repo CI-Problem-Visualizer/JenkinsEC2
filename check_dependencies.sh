334098212781d0dfb2090425c102ba11aa9360a0
Rob Moore <rmoore@gradle.com>
2021-06-26 09:42:30 +0800

    Change dependencies.sh to check_dependencies.sh. It just fails if you don't have the right dependencies installed.

---
+++ check_dependencies.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+which terraform
+which python3
+which pip3
+pip3 show requests
+which aws
+ls ~/.aws/credentials
+
+set +e
--- dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-pip3 install --user requests
-
-set +e
